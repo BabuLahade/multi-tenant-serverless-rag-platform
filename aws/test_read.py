4e0c5572681b5d72d2cb2b25d5068dd59d7bc103
BabuLahade <babulahade@gmail.com>
2026-06-21 19:02:07 +0530

    test read py is working

---
+++ aws/test_read.py
@@ -0,0 +1,14 @@
+
+from vector_repository import get_chunks
+
+chunks = get_chunks("fintech")
+
+print(f"Found {len(chunks)} chunks")
+
+for chunk in chunks:
+
+    print("-" * 50)
+
+    print(chunk["chunk_id"])
+
+    print(chunk["text"][:200])
