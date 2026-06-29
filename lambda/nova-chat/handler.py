b3cea7cb8a61b82eed8bb2496cf4c240d8aad333
BabuLahade <babulahade@gmail.com>
2026-06-29 17:50:34 +0530

    handler file created for nova chat

--- lambda/nova-chat/handler.py
+++ lambda/nova-chat/handler.py
@@ -1,6 +1,30 @@
+import json
+
+from rag import ask
+
+
 def handler(event, context):
+
+    body = json.loads(
+        event["body"]
+    )
+
+    question = body["message"]
+
+    client_id = body["client_id"]
+
+    result = ask(
+        client_id,
+        question
+    )
 
     return {
         "statusCode": 200,
-        "body": "Lambda Working"
+
+        "headers": {
+            "Content-Type":
+            "application/json"
+        },
+
+        "body": json.dumps(result)
     }
