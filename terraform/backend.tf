f0cd2bf75ff8f18268b2d808f400ac89eff2bd9f
BabuLahade <babulahade@gmail.com>
2026-08-13 18:35:07 +0530

    new aws account configuration added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,19 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "nova-rag-tf-state-babulahade"
+#     key            = "dev/terraform.tfstate"
+#     region         = "ap-south-1"
+#     dynamodb_table = "nova-rag-tf-locks"
+#     encrypt        = true
+#   }
+# }
+
 terraform {
   backend "s3" {
-    bucket         = "nova-rag-tf-state-babulahade"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "nova-rag-tf-locks"
-    encrypt        = true
+    bucket       = "nova-rag-tf-state-169748358276"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
