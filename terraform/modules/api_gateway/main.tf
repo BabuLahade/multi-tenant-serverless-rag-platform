c6fb1252190bb33f5e1d1675d3250890cd45e191
BabuLahade <babulahade@gmail.com>
2026-09-04 19:14:00 +0530

    onboard options are added

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -364,3 +364,50 @@ resource "aws_lambda_permission" "apigw_onboard" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.rag_api.execution_arn}/*/*"
 }
+
+resource "aws_api_gateway_method" "onboard_options" {
+  rest_api_id   = aws_api_gateway_rest_api.rag_api.id
+  resource_id   = aws_api_gateway_resource.onboard.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "onboard_options" {
+  rest_api_id = aws_api_gateway_rest_api.rag_api.id
+  resource_id = aws_api_gateway_resource.onboard.id
+  http_method = aws_api_gateway_method.onboard_options.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "onboard_options" {
+  rest_api_id = aws_api_gateway_rest_api.rag_api.id
+  resource_id = aws_api_gateway_resource.onboard.id
+  http_method = aws_api_gateway_method.onboard_options.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "onboard_options" {
+  rest_api_id = aws_api_gateway_rest_api.rag_api.id
+  resource_id = aws_api_gateway_resource.onboard.id
+  http_method = aws_api_gateway_method.onboard_options.http_method
+  status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+  depends_on = [
+    aws_api_gateway_integration.onboard_options
+  ]
+}
