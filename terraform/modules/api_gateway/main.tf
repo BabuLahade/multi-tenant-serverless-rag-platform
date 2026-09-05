faa89535bd0cd947506cc1e9b028146c7bee2df1
BabuLahade <babulahade@gmail.com>
2026-09-05 10:32:40 +0530

    APi Gateway Limit added for chatbot request

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -492,3 +492,24 @@ resource "aws_api_gateway_integration_response" "onboard_options" {
     aws_api_gateway_integration.onboard_options
   ]
 }
+
+# Attach CORS headers to API Gateway default error responses (e.g., 429 Throttled)
+resource "aws_api_gateway_gateway_response" "cors_4xx" {
+  rest_api_id   = aws_api_gateway_rest_api.rag_api.id
+  response_type = "DEFAULT_4XX"
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Content-Type,x-api-key,Authorization'"
+  }
+}
+
+resource "aws_api_gateway_gateway_response" "cors_5xx" {
+  rest_api_id   = aws_api_gateway_rest_api.rag_api.id
+  response_type = "DEFAULT_5XX"
+
+  response_parameters = {
+    "gatewayresponse.header.Access-Control-Allow-Origin"  = "'*'"
+    "gatewayresponse.header.Access-Control-Allow-Headers" = "'Content-Type,x-api-key,Authorization'"
+  }
+}
